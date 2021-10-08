afd241d22710deba2b91b4eace93954360b6e181
fahmiazis <fahmiazis797@gmail.com>
2021-10-08 19:00:11 +0700

    update route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,6 @@ const user = require('../controllers/user')
 route.post('/add', user.addUser)
 route.get('/get', user.getUser)
 route.delete('/del', user.deleteUser)
+route.get('/export', user.exportSqlUser)
 
 module.exports = route
